fb45eb2d56a4281a556f10646dd6347012318a09
Arthur CAUCHY <acauchy@e1r2p16.42.fr>
2019-01-24 17:29:54 +0100

    now keeps form data after an error

--- views/signin.php
+++ views/signin.php
@@ -8,8 +8,8 @@ if (isset($error)) {
 ?>
 <p>Sign in to your account.</p>
 <form method="POST" action="index.php?page=signin">
-    <input type="text" name="username"/>
-    <input type="password" name="passwd"/>
+    <input type="text" name="username" placeholder="username" value="<?php if (isset($_POST["username"])) echo $_POST["username"] ?>"/>
+    <input type="password" name="passwd" placeholder="password" value="<?php if (isset($_POST["passwd"])) echo $_POST["passwd"] ?>"/>
     <input type="submit" value="OK"/>
 </form>
 </body>
